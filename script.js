a5ad36c28bffd6c5b72637d5aea8cf85ba4f486f
Sandra <sandrabalog@rocketmail.com>
2022-10-19 22:14:42 +0200

    more exercise

--- script.js
+++ script.js
@@ -36,7 +36,6 @@
 //     alert("Odgovor je tačan");
 //   }
 
-
 // Zadatak 3
 
 // Traženje parnog broja
@@ -149,7 +148,7 @@
 // Napraviti promenljivu korisničkoIme i promenljivu lozinka i dodeliti im neku string vrednost.
 // Pristup je dozvoljen ukoliko je korisničko ime "administrator" i ukoliko je lozinka "enigma".
 // Izvršiti proveru i ispisati "Dobrodošli" ukoliko je pristup dozvoljen ili "Pristup odbijen" ukoliko su
-// uneti netačni podaci. 
+// uneti netačni podaci.
 
 // var korisnickoIme = prompt("Unesi korisničko ime:");
 // var lozinka = prompt("Unesi lozinki:");
@@ -180,52 +179,119 @@
 // (false && true) = 0
 // (false && false) = 0
 
-// 1(!(5>6 || 6>5));    
-// 2(!(5>6 && 6>5))     
-// 3( 3>2 || 2>3 || 1>2)    
-// 4(3>2 && 2>3 && 1>2)      
-// 5(!( 3>2 || 2>3 || 1>2))     
+// 1(!(5>6 || 6>5));
+// 2(!(5>6 && 6>5))
+// 3( 3>2 || 2>3 || 1>2)
+// 4(3>2 && 2>3 && 1>2)
+// 5(!( 3>2 || 2>3 || 1>2))
 // 6(!(3>2 && 2>3 && 1>2))
 
 // // rešenje
 // // false || false -> false***
 // // true && true -> true****
-console.log (!(false|| true)); //true
-console.log(!(false && true)); // false ! = true
-console.log(true || false || false); // true
-console.log(true && false && false); // false
-console.log(!(true || false || false)); // true ! = false ???? u rešenju je true dal je greška
-console.log(!(true && false && false)); // false ! = true
-
+// console.log (!(false|| true)); //true
+// console.log(!(false && true)); // false ! = true
+// console.log(true || false || false); // true
+// console.log(true && false && false); // false
+// console.log(!(true || false || false)); // true ! = false ???? u rešenju je true dal je greška
+// console.log(!(true && false && false)); // false ! = true
 
 // Zadatak 10
 
 // Napraviti tri niza od kojih prvi treba da sadrži kolekciju stringova , drugi kolekciju brojeva dok
 // treći treba da sadrži kolekciju promenljivih.
 
-var stringovi = ["Pekinezer", "Doberman", "Pudlica", "Mops"];
-var brojevi = [1,2,3,4,5,6];
-var promenjive = [stringovi, brojevi];
+// var stringovi = ["Pekinezer", "Doberman", "Pudlica", "Mops"];
+// var brojevi = [1,2,3,4,5,6];
+// var promenjive = [stringovi, brojevi];
 
-console.log(brojevi.length);
-console.log(stringovi.length);
-console.log(stringovi[2]);
-console.log(brojevi[3]);
+// console.log(brojevi.length);
+// console.log(stringovi.length);
+// console.log(stringovi[2]);
+// console.log(brojevi[3]);
 
+// Zadatak 11
 
+// NAPRAVITI OBJEKAT OSOBA.
+// OBJEKAT TREBA DA SADRZI: IME, PREZIME, GODINU RODJENJA I PREBIVALISTE
 
+// var osoba = {
+//   ime: "Sandra",
+//   prezime: "Balog",
+//   godinaRodjenja: 1987,
+//   prebivalište: "Novi Sad"
+// }
+// console.log(osoba.prebivalište);
+// console.log("Zovem se " + osoba.ime + " prezivam se " + osoba.prezime + " rodjena sam " + osoba.godinaRodjenja + " u " + osoba.prebivalište);
 
+// Zadatak 12
+//object  array
 
+// var imena = [ "Sandra", "Pera", "Djoka", "Marina"];
 
+// console.log(imena[1]);
+// console.log(typeof imena);
+// console.log(imena.length);
 
+// Zadatak 13
 
+// functions
 
+// function showMyName (name) {
+//   console.log("My name is " + name);
+// }
 
-// 1.false || true = ! true =>  false;
-// 2.false && true = ! false =>  true;
-// 3.true || false || false = true;
-// 4.true && false && false = false;
-// 5.true || false || false = true;
-// 6.true && false && false= ! false => true  
+// showMyName("Kosta");
+// showMyName("Marica");
+// showMyName("Lazar");
 
+// Zadatak 14
+// template literals
 
+// var name1 = "Pera";
+// console.log(`My name is ${name1}`);
+
+// Zadatak 15 *** Interesantan zadatak
+
+// exercise problem : swap two numbers
+
+// var apple = 10;
+// var orange = 20;
+// var temp = apple;
+
+// apple = orange;
+// orange = temp;
+// console.log(apple, orange);
+
+// Zadatak 16
+
+// Problem : Our grading system
+// take Number
+// if the mark is in between 80 - 100 print A+
+// if the mark is in between 70 - 79 print A
+// if the mark is in between 60 - 69 print A-
+// if the mark is in between 50 - 59 print B
+// if the mark is in between 40 - 49 print C
+// if the mark is in between 33 - 39 print D
+// if the mark is in between 0 - 32 print F
+// Also point out the invalid marks
+
+var mark = 59;
+
+if (mark < 0 || mark > 100) {
+  console.log('Invalid mark');
+} else if (mark >= 80 && mark <= 100) {
+  console.log('A+');
+} else if (mark >= 70 && mark <= 79) {
+  console.log('A');
+} else if (mark >= 60 && mark <= 69) {
+  console.log('A-');
+} else if (mark >= 50 && mark <= 59) {
+  console.log('B');
+} else if (mark >= 40 && mark <= 49) {
+  console.log('C');
+} else if (mark >= 33 && mark <= 39) {
+  console.log('D');
+} else if (mark > 0 && mark <= 32) {
+  console.log('F');
+}
