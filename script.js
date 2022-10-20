94d7c8c216a86ce0c908af1fb2dbc611a5a834a1
Sandra <sandrabalog@rocketmail.com>
2022-10-20 07:14:12 +0200

    new change

--- script.js
+++ script.js
@@ -324,3 +324,16 @@
 //     ? 'Fizz'
 //     : 'Nothing'
 // );
+
+// Zadatak 18 
+
+// Write a JavaScript program to display the current day and time in the following format.
+
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+var today = new Date();
+var day = today.getDate();
+var daylist = ["Monday", "Thusdey", "Wednesday", "Thursday", "Friday","Saturday","Sunday"];
+
+console.log("Thoday is" + daylist[day]);
